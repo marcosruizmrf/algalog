263925c4c54b11220190db586ff95fdd88ac9ccc
Marcos Freire <marcosruizmrf@outlook.com>
2021-08-11 17:34:31 -0300

    Connecting the database and creating cliente table

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,13 +1,18 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@Entity
 public class Cliente {
 	
+	@Id
 	private Long id;
 	private String nome;
 	private String email;
