d83bfe817feaa81ebf2c3f562328e3b89c41f34c
Marcos Freire <marcosruizmrf@outlook.com>
2021-08-11 18:08:23 -0300

    Creating interface Repository, method GET findAll, GET findByName and GET findByContaining

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -5,21 +5,37 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	//@PersistenceContext
+	//private EntityManager manager;
+	
+	@Autowired // ou então gerar um construtor
+	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();   //JPQL consulta do javax na tabela cliente
+		return clienteRepository.findAll();
+		//manager.createQuery("from Cliente", Cliente.class)
+		//.getResultList();   //JPQL consulta do javax na tabela cliente	
 	}
-
+	
+	@GetMapping("/clientes/nome")
+	public List<Cliente> listarNome() {
+		return clienteRepository.findByNome("João da Silva");	
+	}
+	
+	@GetMapping("/clientes/contem")
+	public List<Cliente> listarNomeContain() {
+		return clienteRepository.findByNomeContaining("João");	
+	}
+	
 }
