c9c210af8a18f71e6c9d67635a41ebd09b164676
Marcos Freire <marcosruizmrf@outlook.com>
2021-08-11 17:51:45 -0300

    Creating a method GET using JPQL in cliente table

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("João");
-		cliente1.setEmail("joao@outlook.com");
-		cliente1.setTelefone("11 97777-2222");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setEmail("maria@outlook.com");
-		cliente2.setTelefone("11 98888-2222");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();   //JPQL consulta do javax na tabela cliente
 	}
 
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,21 +1,37 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
 	private String nome;
+	@Column
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 }
